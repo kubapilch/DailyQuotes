1dde18ef3e7138a230994eb4ba99b2c537b7cc34
kubapilch <kuba.pilch2001@gmail.com>
2017-11-23 22:52:13 +0100

    Clean up

--- Daily Quote/AppDelegate.swift
+++ Daily Quote/AppDelegate.swift
@@ -26,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
         return true
     }
 
